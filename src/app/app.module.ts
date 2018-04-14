3be2ae953ed308fe2008d3febd265f712ceb26e6
Edgar Jakousi Rangel Martinez <edgar.rangel@mexicoss.com>
2018-04-13 20:48:53 -0500

    Layout de la aplicaciópn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './components/account/account.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
